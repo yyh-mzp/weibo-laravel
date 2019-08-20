422e89899c74a25a02fbc68cc3e41d87b32e7dc0
yuhang <13098801456@163.com>
2019-08-20 05:53:59 +0000

    用户注册页面

--- routes/web.php
+++ routes/web.php
@@ -13,5 +13,5 @@
 Route::get('/','StaticPagesController@home')->name('home');
 Route::get('/help', 'StaticPagesController@help')->name('help');
 Route::get('/about', 'StaticPagesController@about')->name('about');
-
+Route::get('/signup','UserController@create')->name('signup');
 
