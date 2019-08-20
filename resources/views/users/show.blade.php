6b05cbe397f8ae4cdcce0eecd3787544501027f3
yuhang <13098801456@163.com>
2019-08-20 09:08:29 +0000

    ‘增加CSRF验证’

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -7,6 +7,7 @@
             <div class="col-md-12">
                 <div class="offset-md-2 col-md-8">
                     <section class="user_info">
+                        @include('shared._user_info', ['user' => $user])
                     </section>
                 </div>
             </div>
