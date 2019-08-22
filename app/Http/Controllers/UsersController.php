34de6cf3a4b99034bdc7bf17a2900f0a40618c9a
yuhang <13098801456@163.com>
2019-08-22 13:30:46 +0000

    更改用户资料

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -19,6 +19,7 @@ class UsersController extends Controller
         return view('users.show',compact('user'));
     }
 
+    //注册自动登陆
     public function store(Request $request)
     {
        $this->validate($request,[
@@ -36,4 +37,34 @@ class UsersController extends Controller
        session()->flash('success','欢迎，您将在这里开启一段新的旅程');
        return redirect()->route('users.show',[$user]);
     }
+
+    //编辑用户
+    public function edit(User $user)
+    {
+        return view('users.edit',compact('user'));
+    }
+
+    public function update(User $user,Request $request)
+    {
+
+         $this->validate($request,[
+             'name'=>'required|max:50',
+             'password'=>'nullable|confirmed',
+         ]);
+         /*$user->update([
+             'name'=>$request->name,
+             'password'=>bcrypt($request->password)
+         ]);*/
+         $data = [];
+         $data['name'] = $request->name;
+         if($request->password)
+         {
+             $data['password']=$request->password;
+         }
+         $user->update($data);
+         session()->flash('success','个人资料更新成功');
+
+         return redirect()->route('users.show',$user);
+    }
+
 }
