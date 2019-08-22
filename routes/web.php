1ab0de9c23cfdc19e816e2975e30291363407bf2
yuhang <13098801456@163.com>
2019-08-22 09:56:35 +0000

    创建会话

--- routes/web.php
+++ routes/web.php
@@ -17,3 +17,6 @@ Route::get('/signup','UsersController@create')->name('signup');
 
 Route::resource('users','UsersController');
 
+Route::get('/login','SessionsController@create')->name('login');
+Route::post('/login', 'SessionsController@store')->name('login');
+Route::delete('/logout', 'SessionsController@destroy')->name('logout');
